1173200f3e0a75939163c6ebaa4a8db272663efc
Cody Canning <cocanning11@gmail.com>
2018-12-10 01:02:24 -0500

    day9 pt 2

--- src/aoc/day9.clj
+++ src/aoc/day9.clj
@@ -1,26 +1,6 @@
 (ns aoc.day9
   (:import [java.util ArrayList List]))
 
-;; (def input [428 72061])
-
-
-
-#_(defrecord Board [marbles]
-  Circular
-  (insert [this i delta v]
-    (let [size      (count marbles)
-          j         (mod (+ i delta) size)
-          [bef aft] (split-at j marbles)
-          marbs     (concat bef [v] aft)
-          ret       (if (= j 0)
-                      [size v (conj (vec marbles) v)]
-                      [j v marbs])]
-      ret))
-  (delete [this i delta]
-    (let [j         (mod (+ i delta) (count marbles))
-          [bef aft] (split-at j marbles)
-          marbs     (concat bef (rest aft))]
-      [j (nth marbles j) marbs])))
 
 (def #^java.util.ArrayList marbles
   (ArrayList. [0]))
@@ -30,7 +10,6 @@
   (let [j   (mod (+ i delta) (.size marbles))
         old (.get marbles j)]
     (.remove marbles (int j))
-    ;; (println marbles)
     [j old (.get marbles j)]))
 
 (defn insert
@@ -39,25 +18,7 @@
         j    (mod (+ i delta) size)
         j    (if (= j 0) size j)]
     (.add marbles j v)
-    ;; (println marbles)
     [j nil v]))
-
-
-
-;; (def #^java.util.ArrayList temp (java.util.ArrayList. ["foo"]))
-;; (defn foo []
-;;   (let [i (mod 3 3)]
-;;     (.get temp 0)
-;;     (.remove temp (int i))))
-
-
-;; marbles
-#_(insert 0 2 1)
-#_(insert 1 2 2)
-#_(insert 1 2 3)
-#_(insert 3 2 4)
-;; (delete 3 -2)
-
 
 
 (defn next-turn
@@ -79,8 +40,7 @@
                                 #(insert curr-ix 2 turn))
         [ix old v]            (action)]
     (if turn-23?
-      (do #_(println (:turn game) "score" (+ turn old))
-          (next-turn game [ix v] (+ turn old)))
+      (next-turn game [ix v] (+ turn old))
       (next-turn game [ix v] 0))))
 
 
@@ -91,18 +51,17 @@
   (let [game {:players (into {} (for [i (range players)] [(inc i) 0]))
               :player     1
               :turn       1
-              ;; :board      (doto (ArrayList.) (.add 0))
               :marble     [0 0]
               :max-points last-marb-points}]
     (loop [g (do-turn game)]
       (when (= 0 (mod (:turn g) 10000))
         (println (:turn g)))
-      ;; (println (:turn g))
-      ;; (println (:turn g) (vec marbles))
       (if (<= last-marb-points (:turn g))
         (high-score g)
         (recur (do-turn g))))))
 
+(defn -main [& args]
+  (println (play-game 428 (* 100 72061))))
 
 
 #_(play-game 10 1618)
